efdc9d067d34fffe669511486129e2e051a000f4
Jor Seng <v-jorsengtan@microsoft.com>
2021-04-09 01:43:45 +0000

    update aztfmod version in landingzones

--- landingzones/caf_solutions/landingzone.tf
+++ landingzones/caf_solutions/landingzone.tf
@@ -1,8 +1,7 @@
 module "caf" {
-  # source  = "aztfmod/caf/azurerm"
-  # version = "~>5.2.0"
-  source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=5.3.0_preview"
-  
+  source  = "aztfmod/caf/azurerm"
+  version = "~>5.3.0"  
+
 
   azuread_api_permissions      = var.azuread_api_permissions
   azuread_apps                 = var.azuread_apps
